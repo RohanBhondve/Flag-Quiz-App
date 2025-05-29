40adc231a18859c21edb4aa7f460336b33373f9b
Rohan <bhondverohan103@gmail.com>
2025-05-29 21:56:11 +0530

    DAO class added

---
+++ app/src/main/java/com/learning/flagquiz/database/FlagsDao.kt
@@ -0,0 +1,45 @@
+package com.learning.flagquiz.database
+
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import com.learning.flagquiz.model.FlagsModel
+import com.techmania.flagquizwithsqlitedemo.DatabaseCopyHelper
+
+class FlagsDao {
+    //this gives us 5 questions
+    fun randomFiveRecords(helper: DatabaseCopyHelper):ArrayList<FlagsModel>{
+        val recordList = ArrayList<FlagsModel>()
+        val database: SQLiteDatabase = helper.readableDatabase
+        val cursor: Cursor = database.rawQuery("SELECT * FROM flags ORDER BY Random() LIMIT 10",null)
+
+        val idIndex = cursor.getColumnIndex("flag_id")
+        val countryNameIndex = cursor.getColumnIndex("country_name")
+        val flagNameIndex = cursor.getColumnIndex("flag_name")
+
+        while(cursor.moveToNext()){
+            val record = FlagsModel(cursor.getInt(idIndex),cursor.getString(countryNameIndex),cursor.getString(flagNameIndex))
+            recordList.add(record)
+        }
+        cursor.close()
+        return recordList
+    }
+
+    //this gives us 3 incorrect options
+    fun randomThreeRecords(helper: DatabaseCopyHelper,id: Int):ArrayList<FlagsModel>{
+        val recordList = ArrayList<FlagsModel>()
+        val database: SQLiteDatabase = helper.readableDatabase
+        val cursor: Cursor = database.rawQuery("SELECT * FROM flags WHERE id != ? ORDER BY Random() LIMIT 3",arrayOf(id.toString()))
+
+        val idIndex = cursor.getColumnIndex("flag_id")
+        val countryNameIndex = cursor.getColumnIndex("country_name")
+        val flagNameIndex = cursor.getColumnIndex("flag_name")
+
+        while(cursor.moveToNext()){
+            val record = FlagsModel(cursor.getInt(idIndex),cursor.getString(countryNameIndex),cursor.getString(flagNameIndex))
+            recordList.add(record)
+        }
+        cursor.close()
+        return recordList
+    }
+
+}
---
+++ app/src/main/java/com/learning/flagquiz/model/FlagsModel.kt
@@ -0,0 +1,7 @@
+package com.learning.flagquiz.model
+
+data class FlagsModel(
+    val id:Int,
+    val countryName:String,
+    val flagName:String
+)
