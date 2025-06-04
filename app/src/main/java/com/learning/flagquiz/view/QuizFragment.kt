c3ad577698f1b7bf7466aeadae8866e39e339f80
Rohan <bhondverohan103@gmail.com>
2025-06-04 19:17:54 +0530

    correct and wrong answers controlled

--- app/src/main/java/com/learning/flagquiz/view/QuizFragment.kt
+++ app/src/main/java/com/learning/flagquiz/view/QuizFragment.kt
@@ -1,11 +1,13 @@
 package com.learning.flagquiz.view
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.core.content.res.ResourcesCompat
 import com.learning.flagquiz.R
 import com.learning.flagquiz.database.FlagsDao
@@ -36,34 +38,28 @@ class QuizFragment : Fragment() {
 
         flagList = dao.randomFiveRecords(DatabaseCopyHelper(requireActivity()))
 
-        for(flags in flagList){
-            Log.d("flags",flags.id.toString())
-            Log.d("flags",flags.countryName.toString())
-            Log.d("flags",flags.flagName.toString())
-            Log.d("flags","**********************")
-        }
-
         showData()
 
         fragQuizBinding.option1.setOnClickListener {
-
+            answerControl(fragQuizBinding.option1)
         }
 
         fragQuizBinding.option2.setOnClickListener {
-
+            answerControl(fragQuizBinding.option2)
         }
 
         fragQuizBinding.option3.setOnClickListener {
-
+            answerControl(fragQuizBinding.option3)
         }
 
         fragQuizBinding.option4.setOnClickListener {
-
+            answerControl(fragQuizBinding.option4)
         }
 
         fragQuizBinding.buttonNext.setOnClickListener{
             question++
             showData()
+            setOptionToDefault()
         }
 
         return fragQuizBinding.root
@@ -98,6 +94,55 @@ class QuizFragment : Fragment() {
         fragQuizBinding.option3.text = options[2].countryName
         fragQuizBinding.option4.text = options[3].countryName
 
+    }
+
+    private fun answerControl(button: Button){
+        val clickedOption = button.text.toString()
+        val correctAns = correctFlag.countryName
+
+        if(clickedOption == correctAns){
+            correct++
+            fragQuizBinding.tvCorrect.text = correct.toString()
+            button.setBackgroundColor(resources.getColor(R.color.correct,requireActivity().theme))
+        }
+        else{
+            incorrect++
+            fragQuizBinding.tvIncorrect.text = incorrect.toString()
+            button.setBackgroundColor(resources.getColor(R.color.incorrect,requireActivity().theme))
+
+            when(correctAns){
+
+                fragQuizBinding.option1.text -> fragQuizBinding.option1.setBackgroundColor(resources.getColor(R.color.correct,requireActivity().theme))
+                fragQuizBinding.option2.text -> fragQuizBinding.option2.setBackgroundColor(resources.getColor(R.color.correct,requireActivity().theme))
+                fragQuizBinding.option3.text -> fragQuizBinding.option3.setBackgroundColor(resources.getColor(R.color.correct,requireActivity().theme))
+                fragQuizBinding.option4.text -> fragQuizBinding.option4.setBackgroundColor(resources.getColor(R.color.correct,requireActivity().theme))
+
+            }
+        }
+
+        fragQuizBinding.option1.isClickable = false
+        fragQuizBinding.option2.isClickable = false
+        fragQuizBinding.option3.isClickable = false
+        fragQuizBinding.option4.isClickable = false
+    }
+
+    private fun setOptionToDefault(){
+        fragQuizBinding.option1.apply {
+            setBackgroundColor(resources.getColor(R.color.optionsbg,requireActivity().theme))
+            isClickable = true
+        }
+        fragQuizBinding.option2.apply {
+            setBackgroundColor(resources.getColor(R.color.optionsbg,requireActivity().theme))
+            isClickable = true
+        }
+        fragQuizBinding.option3.apply {
+            setBackgroundColor(resources.getColor(R.color.optionsbg,requireActivity().theme))
+            isClickable = true
+        }
+        fragQuizBinding.option4.apply {
+            setBackgroundColor(resources.getColor(R.color.optionsbg,requireActivity().theme))
+            isClickable = true
+        }
     }
 
 }
